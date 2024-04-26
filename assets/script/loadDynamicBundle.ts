b5824819a69d4b8e98b4151f41a0a41e6c11e866
Abhishek Rawat <abhishek.rawat@chicmic.co.in>
2024-04-26 10:01:02 +0530

    remote loading working properly

---
+++ assets/script/loadDynamicBundle.ts
@@ -0,0 +1,59 @@
+import { _decorator, assetManager, Component, Node, Sprite, SpriteFrame } from "cc";
+const { ccclass, property } = _decorator;
+
+@ccclass("loadDynamicBundle")
+export class loadDynamicBundle extends Component {
+    @property({ type: Sprite })
+    img: Sprite = null;
+    protected onLoad(): void {
+        // assetManager.loadBundle("localhost:8000", (err, bundle) => {
+        //     // if (err) {
+        //     //     console.log("asset manager error", err);
+        //     //     return;
+        //     // }
+        //     // console.log("bundle", bundle);
+        //     // // bundle.load("resources/person/person1/spriteFrame", SpriteFrame, null, (err, spriteFrame) => {
+        //     // //     if (err) {
+        //     // //         console.log("bundle error", err);
+        //     // //         return;
+        //     // //     }
+        //     // //     this.img.spriteFrame = spriteFrame;
+        //     // // });
+        // });
+    }
+    start() {
+        assetManager.loadBundle("http://192.180.0.76:8000", (err, bundle) => {
+            if (err) {
+                console.log("asset manager error", err);
+                return;
+            }
+            console.log("bundle", bundle);
+            bundle.load("images/person3/spriteFrame", SpriteFrame, null, (err, spriteFrame) => {
+                if (err) {
+                    console.error("bundle error", err);
+                    return;
+                }
+                console.log(spriteFrame);
+                this.img.spriteFrame = spriteFrame;
+            });
+        });
+    }
+
+    update(deltaTime: number) {}
+
+    // onClick() {
+    // assetManager.loadBundle("localhost:3000/bundle1", (err, bundle) => {
+    //     if (err) {
+    //         console.log("asset manager error", err);
+    //         return;
+    //     }
+    //     bundle.load("resources/person1/spriteFrame", SpriteFrame, null, (err, spriteFrame) => {
+    //         if (err) {
+    //             console.log("bundle error", err);
+    //             return;
+    //         }
+    //         this.img.spriteFrame = spriteFrame;
+    //     });
+    // });
+    // }
+}
